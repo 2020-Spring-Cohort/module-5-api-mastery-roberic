c31616525086aa49166bbfd22a3669993106f1f1
Eric Drexler <ericdrxlr@gmail.com>
2020-03-22 14:54:22 -0400

    set up main class with imports from components and function structure

--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -1,7 +1,33 @@
-export default function(){
-    document.querySelector('#app').innerHTML=
-    `
-    <h2>Your Awesome API Collection</h2>
-    <h3>It's gonna be great!</h3>
-    `
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+import apiActions from './api/apiActions';
+
+export default pageBuild
+
+function pageBuild()
+{
+    header()
+    homepage()
+    footer()
+    AboutUs()
+}
+
+function header()
+{
+    const header = document.querySelector('#header')
+    header.innerHTML = Header()
+}
+
+function footer()
+{
+    const footer = document.querySelector('#footer')
+    footer.innerHTML = Footer()
+}
+
+function homepage()
+{
+    const homepageButton = document.querySelector('#header')
+    header.innerHTML = Header()
 }
