1b7ff12c705a771ef94d546c557a2a09b63b7c7a
Eric Drexler <ericdrxlr@gmail.com>
2020-03-22 14:17:47 -0400

    updated generic repository class with necessart set up for crud

--- backend-api/api-collection/Repositories/Repository.cs
+++ backend-api/api-collection/Repositories/Repository.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_collection.Repositories
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        public DbContext db;
+        public Repository(DbContext db)
+        {
+            this.db = db;
+        }
+        public int Count()
+        {
+            return db.Set<T>().Count();
+        }
+        public void Create(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            db.Set<T>().Update(entity);
+            db.SaveChanges();
+        }
+        public void Delete(T entity)
+        {
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }  
+        public void Save()
+        {
+            db.SaveChanges();
+        } 
     }
 }
